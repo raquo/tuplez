9345bd3436723d0ba6f254b0ffa366f28089e4ef
Iurii Malchenko <i@yurique.com>
2026-02-27 23:41:27 +0100

    don't test decompose in scala-2

--- project/TupleCompositionTestGenerator.scala
+++ project/TupleCompositionTestGenerator.scala
@@ -23,32 +23,40 @@ class TupleCompositionTestGenerator(sourceManaged: File, to: Int, testConcats: B
 
       enter("""@Test def `Unit+Unit`(): Unit = {""")("}") {
         println("""assertEquals("composed should match", unit, TupleComposition.compose(unit, unit))""")
-        println(s"""assertEquals("decomposed should match", (unit, unit), $summon[Decompose[Unit, Unit]].decompose(unit))""")
-        println(s"""assertEquals("decomposed should match", (unit, unit), $summon[Composition[Unit, Unit]].decompose(unit))""")
+        if (scala3) {
+          println(s"""assertEquals("decomposed should match", (unit, unit), $summon[Decompose[Unit, Unit]].decompose(unit))""")
+          println(s"""assertEquals("decomposed should match", (unit, unit), $summon[Composition[Unit, Unit]].decompose(unit))""")
+        }
       }
 
       println()
 
       enter("""@Test def `scalar+scalar`(): Unit = {""")("}") {
         println("""assertEquals("composed should match", ("1", "2"), TupleComposition.compose("1", "2"))""")
-        println(s"""assertEquals("decomposed should match", ("1", "2"), $summon[Decompose[String, String]].decompose(Tuple2("1", "2")))""")
-        println(s"""assertEquals("decomposed should match", ("1", "2"), $summon[Composition[String, String]].decompose(Tuple2("1", "2")))""")
+        if (scala3) { // TODO: scala-2 is not able to figure it out (the decompose.Composed type)
+          println(s"""assertEquals("decomposed should match", ("1", "2"), $summon[Decompose[String, String]].decompose(Tuple2("1", "2")))""")
+          println(s"""assertEquals("decomposed should match", ("1", "2"), $summon[Composition[String, String]].decompose(Tuple2("1", "2")))""")
+        }
       }
 
       println()
 
       enter("""@Test def `scalar+unit`(): Unit = {""")("}") {
         println("""assertEquals("composed should match", "1", TupleComposition.compose("1", unit))""")
-        println(s"""assertEquals("decomposed should match", ("1", unit), $summon[Decompose[String, Unit]].decompose("1"))""")
-        println(s"""assertEquals("decomposed should match", ("1", unit), $summon[Composition[String, Unit]].decompose("1"))""")
+        if (scala3) {
+          println(s"""assertEquals("decomposed should match", ("1", unit), $summon[Decompose[String, Unit]].decompose("1"))""")
+          println(s"""assertEquals("decomposed should match", ("1", unit), $summon[Composition[String, Unit]].decompose("1"))""")
+        }
       }
 
       println()
 
       enter("""@Test def `unit+scalar`(): Unit = {""")("}") {
         println("""assertEquals("composed should match", "2", TupleComposition.compose(unit, "2"))""")
-        println(s"""assertEquals("decomposed should match", (unit, "2"), $summon[Decompose[Unit, String]].decompose("2"))""")
-        println(s"""assertEquals("decomposed should match", (unit, "2"), $summon[Composition[Unit, String]].decompose("2"))""")
+        if (scala3) {
+          println(s"""assertEquals("decomposed should match", (unit, "2"), $summon[Decompose[Unit, String]].decompose("2"))""")
+          println(s"""assertEquals("decomposed should match", (unit, "2"), $summon[Composition[Unit, String]].decompose("2"))""")
+        }
       }
 
       println()
@@ -81,8 +89,10 @@ class TupleCompositionTestGenerator(sourceManaged: File, to: Int, testConcats: B
         enter(s"""@Test def `${size1}-tuple+Unit`(): Unit = {""")("}") {
           println(s"""val tuple = ${tupleValue(size1, 100)}""")
           println(s"""assertEquals("composed should match", tuple, TupleComposition.compose(tuple, (): Unit))""")
-          println(s"""assertEquals("decomposed should match", (tuple, unit), $summon[Decompose[${tupleType(size1)}, Unit]].decompose(tuple))""")
-          println(s"""assertEquals("decomposed should match", (tuple, unit), $summon[Composition[${tupleType(size1)}, Unit]].decompose(tuple))""")
+          if (scala3) {
+            println(s"""assertEquals("decomposed should match", (tuple, unit), $summon[Decompose[${tupleType(size1)}, Unit]].decompose(tuple))""")
+            println(s"""assertEquals("decomposed should match", (tuple, unit), $summon[Composition[${tupleType(size1)}, Unit]].decompose(tuple))""")
+          }
         }
         println()
       }
@@ -91,8 +101,10 @@ class TupleCompositionTestGenerator(sourceManaged: File, to: Int, testConcats: B
         enter(s"""@Test def `${size1}-Unit+tuple`(): Unit = {""")("}") {
           println(s"""val tuple = ${tupleValue(size1, 100)}""")
           println(s"""assertEquals("composed should match", tuple, TupleComposition.compose((): Unit, tuple))""")
-          println(s"""assertEquals("decomposed should match", (unit, tuple), $summon[Decompose[Unit, ${tupleType(size1)}]].decompose(tuple))""")
-          println(s"""assertEquals("decomposed should match", (unit, tuple), $summon[Composition[Unit, ${tupleType(size1)}]].decompose(tuple))""")
+          if (scala3) {
+            println(s"""assertEquals("decomposed should match", (unit, tuple), $summon[Decompose[Unit, ${tupleType(size1)}]].decompose(tuple))""")
+            println(s"""assertEquals("decomposed should match", (unit, tuple), $summon[Composition[Unit, ${tupleType(size1)}]].decompose(tuple))""")
+          }
         }
         println()
       }
@@ -102,8 +114,10 @@ class TupleCompositionTestGenerator(sourceManaged: File, to: Int, testConcats: B
           println(s"""val tuple = ${tupleValue(size1, 100)}""")
           println(s"""val expected = (${tupleElements(size1, 100)}, 201)""")
           println(s"""assertEquals("composed should match", expected, TupleComposition.compose(tuple, 201))""")
-          println(s"""assertEquals("decomposed should match", (tuple, 201), $summon[Decompose[${tupleType(size1)}, Int]].decompose(expected))""")
-          println(s"""assertEquals("decomposed should match", (tuple, 201), $summon[Composition[${tupleType(size1)}, Int]].decompose(expected))""")
+          if (scala3) {
+            println(s"""assertEquals("decomposed should match", (tuple, 201), $summon[Decompose[${tupleType(size1)}, Int]].decompose(expected))""")
+            println(s"""assertEquals("decomposed should match", (tuple, 201), $summon[Composition[${tupleType(size1)}, Int]].decompose(expected))""")
+          }
         }
         println()
       }
@@ -114,8 +128,10 @@ class TupleCompositionTestGenerator(sourceManaged: File, to: Int, testConcats: B
             println(s"""val tuple = ${tupleValue(size1, 100)}""")
             println(s"""val expected = (201, ${tupleElements(size1, 100)})""")
             println(s"""assertEquals("composed should match", expected, TupleComposition.compose(201, tuple))""")
-            println(s"""assertEquals("decomposed should match", (201, tuple), $summon[Decompose[Int, ${tupleType(size1)}]].decompose(expected))""")
-            println(s"""assertEquals("decomposed should match", (201, tuple), $summon[Composition[Int, ${tupleType(size1)}]].decompose(expected))""")
+            if (scala3) {
+              println(s"""assertEquals("decomposed should match", (201, tuple), $summon[Decompose[Int, ${tupleType(size1)}]].decompose(expected))""")
+              println(s"""assertEquals("decomposed should match", (201, tuple), $summon[Composition[Int, ${tupleType(size1)}]].decompose(expected))""")
+            }
           }
           println()
         }
@@ -129,8 +145,10 @@ class TupleCompositionTestGenerator(sourceManaged: File, to: Int, testConcats: B
               println(s"""val tuple2 = ${tupleValue(size2, 200)}""")
               println(s"""val expected = (${tupleElements(size1, 100)}, ${tupleElements(size2, 200)})""")
               println(s"""assertEquals("composed should match", expected, TupleComposition.compose(tuple1, tuple2))""")
-              println(s"""assertEquals("decomposed should match", (tuple1, tuple2), $summon[Decompose[${tupleType(size1)}, ${tupleType(size2)}]].decompose(expected))""")
-              println(s"""assertEquals("decomposed should match", (tuple1, tuple2), $summon[Composition[${tupleType(size1)}, ${tupleType(size2)}]].decompose(expected))""")
+              if (scala3) {
+                println(s"""assertEquals("decomposed should match", (tuple1, tuple2), $summon[Decompose[${tupleType(size1)}, ${tupleType(size2)}]].decompose(expected))""")
+                println(s"""assertEquals("decomposed should match", (tuple1, tuple2), $summon[Composition[${tupleType(size1)}, ${tupleType(size2)}]].decompose(expected))""")
+              }
             }
             println()
           }
